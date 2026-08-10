a4511b66a2bded17a6797bc4e8dea43e45f9dff4
Vostaa <not.amellctrl@gmail.com>
2026-08-10 23:23:55 +0700

    OPERASI BILANGAN

---
+++ ARITMATIKA.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aritmatika</title>
+</head>
+<body>
+    <?php
+        $x = 8;
+        $y = 4;
+        $z = null;
+
+        // OPERASI BILANGAN
+        $z = $x + $y;
+        $z = $x - $y;
+        $z = $x / $y;
+        $z = $x * $y;
+        $z = $x % $y;
+        $z = $x ** $y;
+
+        echo $z;
+
+
+
+
+
+
+
+
+    ?>
+</body>
+</html>
