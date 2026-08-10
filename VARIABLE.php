56c88af331a39bbc2f7d5bdf3da0e1f041e55e0e
Vostaa <not.amellctrl@gmail.com>
2026-08-10 19:32:29 +0700

    Variabel dalam  php

---
+++ VARIABLE.php
@@ -0,0 +1,32 @@
+<?php
+    //  VARIABEL DALAM PHP
+
+    // STRING
+    $nama_siswa = "Citra Amelia <br>";
+    $makanan_kesukaan = "Pizza";
+    $harga_makanan = 50000;
+    
+    echo $nama_siswa;
+    echo "hello {$nama_siswa} <br>";
+
+    echo "{$nama_siswa} kamu dari SMKN 1 Surabaya kan??????<br>";
+    echo "Harga {$makanan_kesukaan} yang dibeli {$nama_siswa} adalah: {$harga_makanan} <br>";
+
+    // INT
+    $umur = 16;
+    $pengguna = 5;
+    $quantitas = 9;
+
+    echo $umur;
+    echo $pengguna;
+    echo $harga_makanan;
+
+    // FLOAT
+    $saldo_total = 16.78;
+    $pengguna_presentase = 5.3;
+    $quantitas = 9.9;
+
+
+    // BOOLEAN
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
-echo "hello, aku citra amelia <br>";
-echo "its really good";
+// halo
+/* 
+    hallo
+    ini
+    contoh
+    multiline 
+    comment
+*/
+    echo "halo, aku citra amelia <br>";
+    echo "Aku berasal dari SMKN 1 Surabaya <br>";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"
+    <meta name="viewport">
+    <title>BELAJAR PHP PERTAMA KALI.</title>
+    <a href="VARIABEL.php">BUKA HALAMAN VARIABEL</a>
+</head>
+<body>
+    <button>AKU KUDU NGAMOK REK DANCOK</button>
+</body>
+</html>
