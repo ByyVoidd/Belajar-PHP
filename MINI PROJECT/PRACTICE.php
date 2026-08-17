d229881c4a059635a068efe767f5790ae7d6e481
Vostaa <not.amellctrl@gmail.com>
2026-08-17 11:35:16 +0700

    Perintah kedai kopi

--- MINI PROJECT/PRACTICE.php
+++ MINI PROJECT/PRACTICE.php
@@ -22,7 +22,7 @@
     $harga_akhir = null;
 
     $harga_akhir = $total_produk * $harga;
-
+         
     echo "Kamu telah memesan sebanyak {$total_produk} {$item} dengan harga satuan {$harga} <br>";
     echo "Totalnya adalah: {$harga_akhir}";
 
